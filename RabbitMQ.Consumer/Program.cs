aa884f6820042805a84c3d8b26abc2a81311ece6
Petar Totev <77621401+petar-totev@users.noreply.github.com>
2022-12-09 23:45:26 +0200

    Initial implementation

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+// Consumer
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" Receiver received message '{0}' from Publisher.", message);
+        };
+
+        channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+// Producer
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+        while (true)
+        {
+            var message = Console.ReadLine();
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+            Console.WriteLine(" Publisher sent message '{0}' to Receiver.", message);
+        }
+    }
+}
